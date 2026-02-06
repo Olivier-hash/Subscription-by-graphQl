49e7e061d4fafb555add574669dfe8232301b4c7
olivier Rukundo <rr.olivier01@gmail.com>
2026-02-06 22:29:03 +0100

    added Header component structure

---
+++ my-react-app/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+
+type HeaderProps = {
+  title?: string;
+};
+
+const Header: React.FC<HeaderProps> = ({ title = "My App" }) => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h1 className="header__title">{title}</h1>
+
+        <nav className="header__nav">
+          <ul className="header__list">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
